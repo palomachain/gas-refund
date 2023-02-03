b60c2b3796b91f86c3bdfcd9f59afde29f420317
fieldtheory123 <biyaqiao@hotmail.com>
2023-02-04 00:51:29 +0800

    Add polygon refund scripts

---
+++ functions_polygon.py
@@ -0,0 +1,137 @@
+import polygonscan_api
+import os
+import time
+import datetime
+import pandas as pd
+import numpy as np
+import sys
+current_dir = os.getcwd()
+sys.path.insert(1, os.path.abspath(os.path.join(current_dir, '../../')))
+
+
+def conv_dt_rev(dt_int):
+    """
+    convert datetime format
+    """
+    return datetime.datetime(1970, 1, 1, 0, 0, 0)+datetime.timedelta(seconds=int(dt_int)/1e0)
+
+
+eth = polygonscan_api.PolygonscanConnector()
+
+contract = '0x24B10a62385C2d04F3f04Dd55297ADD7b4502530' # compass evm
+txs = eth.get_normal_transactions(address=contract)
+methodid = '0xeadf4af7'
+
+
+def get_refundlist(fromtime, totime):
+    fromtime_ = pd.to_datetime(fromtime)
+    totime_ = pd.to_datetime(totime)
+
+    refund_list = {}
+
+    try:
+        for tx in txs:
+            if tx['txreceipt_status'] == '1':
+                time_ = conv_dt_rev(tx['timeStamp'])
+                if time_ > fromtime_ and time_ <= totime_:
+                    sender_ = tx['from']
+                    if sender_ not in refund_list.keys():
+                        refund_list[sender_] = int(
+                            tx['gasUsed']) * int(tx['gasPrice'])
+                    else:
+                        refund_list[sender_] += int(tx['gasUsed']) * \
+                            int(tx['gasPrice'])
+                    # print(tx['blockNumber'], time_, sender_, refund_list[sender_])
+                elif time_ < fromtime_:
+                    break
+    except Exception as e:
+        print(e)
+
+    return refund_list
+
+def get_refundlist_nonvalsetupdate(fromtime, totime):
+    fromtime_ = pd.to_datetime(fromtime)
+    totime_ = pd.to_datetime(totime)
+
+    refund_list = {}
+    total_refund = 0
+
+    try:
+        for tx in txs:
+            if tx['txreceipt_status'] == '1' and tx['methodId'] != methodid:
+                time_ = conv_dt_rev(tx['timeStamp'])
+                if time_ > fromtime_ and time_ <= totime_:
+                    sender_ = tx['from']
+                    if sender_ not in refund_list.keys():
+                        refund_list[sender_] = int(tx['gasUsed']) * int(tx['gasPrice'])
+                    else:
+                        refund_list[sender_] += int(tx['gasUsed']) * \
+                            int(tx['gasPrice'])
+                    total_refund += int(tx['gasUsed']) * int(tx['gasPrice'])
+                    # print(tx['blockNumber'], time_, sender_, refund_list[sender_])
+                elif time_ < fromtime_:
+                    break
+    except Exception as e:
+        print(e)
+
+    print('total refund: ',total_refund)
+    return refund_list
+
+
+def get_refundlist_byblock(contract, methodid, fromblock, toblock):
+
+    refund_list = {}
+    txs = eth.get_normal_transactions(address=contract)
+
+    try:
+        for tx in txs:
+            if tx['txreceipt_status'] == '1' and tx['methodId'] == methodid:
+                blockid_ = int(tx['blockNumber'])
+                if blockid_ >= fromblock and blockid_ <= toblock:
+                    sender_ = tx['from']
+                    if sender_ not in refund_list.keys():
+                        refund_list[sender_] = int(
+                            tx['gasUsed']) * int(tx['gasPrice'])
+                    else:
+                        refund_list[sender_] += int(tx['gasUsed']) * \
+                            int(tx['gasPrice'])
+                    # print(tx['blockNumber'], time_, sender_, refund_list[sender_])
+                elif blockid_ < fromblock:
+                    break
+    except Exception as e:
+        print(e)
+
+    return refund_list
+
+
+def get_refundlist_old(fromtime='2022-08-25 15:00:00', totime='2022-08-26 09:00:00', eth_lowercap=0.1, refund_cap=0.15):
+    fromtime_ = pd.to_datetime(fromtime)
+    totime_ = pd.to_datetime(totime)
+
+    refund_list = {}
+    sender_list = []
+    try:
+        for tx in txs:
+            if tx['txreceipt_status'] == '1' and tx['methodId'] == methodid:
+                time_ = conv_dt_rev(tx['timeStamp'])
+                if time_ > fromtime_ and time_ <= totime_:
+                    sender_ = tx['from']
+                    if not sender_ in sender_list:
+                        balance_ = float(eth.get_account_eth(sender_))/1e18
+                        print(tx['blockNumber'], time_, sender_, balance_)
+                        sender_list.append(sender_)
+
+                        if balance_ < eth_lowercap:
+                            refund_list[sender_] = refund_cap - balance_
+
+                elif time_ < fromtime_:
+                    break
+    except Exception as e:
+        print(e)
+
+    return refund_list
+
+
+if __name__ == '__main__':
+    print(get_refundlist())
+    print(get_refundlist_nonvalsetupdate())
---
+++ run_polygon.py
@@ -0,0 +1,110 @@
+import polygonscan_api
+from functions_polygon import get_refundlist, conv_dt_rev
+import sys
+import os
+import datetime
+import sentry_sdk
+from web3 import Web3
+from eth_account import Account
+from web3.gas_strategies.rpc import rpc_gas_price_strategy
+from mixpanel import Mixpanel
+mp = Mixpanel(os.environ['MIXPANEL_TOKEN'])
+
+sentry_sdk.init(
+    dsn=os.environ['SENTRY_DSN'],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+)
+
+current_dir = os.getcwd()
+sys.path.insert(1, os.path.abspath(os.path.join(current_dir, '../../')))
+
+"""
+This script requires the following environment variables:
+NODE: a EVM node https endpoint
+WALLET: refund wallet address
+PRIVATE_KEY: refund wallet private key
+"""
+
+node = os.environ['NODE']
+w3 = Web3(Web3.HTTPProvider(node))
+print('check EVM node connected: ', w3.isConnected())
+
+# fromtime = argv[1] # '2022-08-25 15:00:00'
+# totime = argv[2] # '2022-08-26 09:00:00'
+# eth_lowercap = float(argv[3]) #0.1
+# refund_cap =  float(argv[4]) #0.15
+
+private_key = os.environ['PRIVATE_KEY']
+account_from = Account.from_key(private_key)
+assert account_from.address == os.environ['REFUND_WALLET']  # this is the refund wallet address
+contract_address = "0x83334ef0C6f6396413C508A7762741e9FD8B20E9"  # this is the refund contract
+abi = [{"type": "function", "name": "refund", "stateMutability": "payable", "inputs": [
+    {"name": "receivers", "type": "address[]"}, {"name": "amounts", "type": "uint256[]"}], "outputs": []}]
+
+eth = etherscan_api.EtherscanConnector()
+txs_account = eth.get_normal_transactions(address=account_from.address)
+
+txs_contract = eth.get_normal_transactions(address=contract_address)
+
+fromtime = conv_dt_rev(0)
+for tx in txs_account:
+    if tx["functionName"][0:6] == "refund":
+        fromtime = conv_dt_rev(tx['timeStamp'])
+        break
+
+for tx in txs_contract:
+    if fromtime > conv_dt_rev(tx['timeStamp']):
+        break
+    if tx["functionName"][0:6] == "refund":
+        if fromtime < conv_dt_rev(tx['timeStamp']):
+            fromtime = conv_dt_rev(tx['timeStamp'])
+        break
+
+totime = datetime.datetime.utcnow()
+
+print(fromtime, totime)
+
+
+def send(refund_list, gas=21000, account_from=account_from):
+
+    value = 0
+    address_list = []
+    amount_list = []
+    for receiver in refund_list.keys():
+        value += refund_list[receiver]
+        mp.track(receiver, 'GAS_REFUND', {
+            'TYPE': 'ETH',
+            'VALUE': refund_list[receiver],
+        })
+        address_list.append(w3.toChecksumAddress(receiver))
+        amount_list.append(refund_list[receiver])
+    if value > 0:  # this should be added. if value equals zero, we don't need to run tx.
+        refund_sc = w3.eth.contract(
+            address=contract_address,
+            abi=abi)
+        nonce = w3.eth.get_transaction_count(account_from.address)
+        max_priority_fee = w3.eth.max_priority_fee
+        gas_price = w3.eth.gas_price * 2 + max_priority_fee
+        tx_create = refund_sc.functions.refund(address_list, amount_list).build_transaction(
+            {"value": value, "nonce": nonce, "from": account_from.address, "maxFeePerGas": gas_price,
+             "maxPriorityFeePerGas": max_priority_fee})
+        print(tx_create)
+        signed_tx = w3.eth.account.sign_transaction(
+            tx_create, private_key=private_key)
+        print(signed_tx)
+        tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+        tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+        #print(f"Transaction successful with hash: { tx_receipt.transactionHash.hex() }")
+        print("Transaction successful")
+    else:
+        print('no refund needed')
+
+
+if 1:
+    refund_list = get_refundlist(fromtime, totime)
+    print(refund_list)
+    send(refund_list, gas=21000, account_from=account_from)
